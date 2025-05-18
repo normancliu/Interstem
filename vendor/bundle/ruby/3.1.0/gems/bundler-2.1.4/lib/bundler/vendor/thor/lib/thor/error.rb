7cc9d5656c76a9b89750b127df0d04f2b63a6462
normancliu <normancliu@gmail.com>
2025-05-18 11:31:01 -0700

    Revert "Update error.rb"
    This reverts commit 3bca7cb7f43521bdde24288aa23bda6a5ee10089.

--- vendor/bundle/ruby/3.1.0/gems/bundler-2.1.4/lib/bundler/vendor/thor/lib/thor/error.rb
+++ vendor/bundle/ruby/3.1.0/gems/bundler-2.1.4/lib/bundler/vendor/thor/lib/thor/error.rb
@@ -102,7 +102,7 @@ class Bundler::Thor
   end
 
   if Correctable
-    DidYouMean.correct_error(
+    DidYouMean::SPELL_CHECKERS.merge!(
       'Bundler::Thor::UndefinedCommandError' => UndefinedCommandError::SpellChecker,
       'Bundler::Thor::UnknownArgumentError' => UnknownArgumentError::SpellChecker
     )
